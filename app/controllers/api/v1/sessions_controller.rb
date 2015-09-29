9d04af3ce1270a202fae0d18bb7a18dbd30da634
Thanyaboon Tovorapan <whatineat@gmail.com>
2015-09-29 09:18:48 +0700

    Adds sessions controller create action

---
+++ app/controllers/api/v1/sessions_controller.rb
@@ -0,0 +1,16 @@
+class SessionsController < ApplicationController
+  def create
+    member_password = params[:session][:password]
+    member_email = params[:session][:email]
+    member = member_email.present? && Member.find_by(email: member_email)
+
+    if member.valid_password? member_password
+      sign_in member, store: false
+      member.generate_authentication_token!
+      member.save
+      render json: member, status: 200, location: [:api, member]
+    else
+      render json: { errors: "Invalid email or password" }, status: 422
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :members
+  resources :sessions, :only => [:create, :destroy]
   root 'welcome#index'
 
 

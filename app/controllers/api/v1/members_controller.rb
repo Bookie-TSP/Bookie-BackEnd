ce3a6f0432af0cb8bc85b69fd023cdc8b7fce804
Thanyaboon Tovorapan <whatineat@gmail.com>
2015-10-01 03:10:06 +0700

    Added the member create endpoint

--- app/controllers/api/v1/members_controller.rb
+++ app/controllers/api/v1/members_controller.rb
@@ -4,4 +4,19 @@ class Api::V1::MembersController < ApplicationController
   def show
     respond_with Member.find(params[:id])
   end
+
+  def create
+    member = Member.new(member_params)
+    if member.save
+      render json: member, status: 201, location: [:api, member]
+    else
+      render json: { errors: member.errors }, status: 422
+    end
+  end
+
+  private
+
+    def member_params
+      params.require(:member).permit(:email, :password, :password_confirmation)
+    end
 end
--- spec/controllers/api/v1/members_controller_spec.rb
+++ spec/controllers/api/v1/members_controller_spec.rb
@@ -17,4 +17,42 @@ RSpec.describe Api::V1::MembersController, type: :controller do
     it { should respond_with 200 }
   end
 
+  describe "POST #create" do
+
+    context "when is successfully created" do
+      before(:each) do
+        @member_attributes = FactoryGirl.attributes_for :member
+        post :create, { member: @member_attributes }, format: :json
+      end
+
+      it "renders the json representation for the user record just created" do
+        member_response = JSON.parse(response.body, symbolize_names: true)
+        expect(member_response[:email]).to eql @member_attributes[:email]
+      end
+
+      it { should respond_with 201 }
+    end
+
+    context "when is not created" do
+      before(:each) do
+        #notice I'm not including the email
+        @invalid_member_attributes = { password: "12345678",
+                                     password_confirmation: "12345678" }
+        post :create, { member: @invalid_member_attributes }, format: :json
+      end
+
+      it "renders an errors json" do
+        member_response = JSON.parse(response.body, symbolize_names: true)
+        expect(member_response).to have_key(:errors)
+      end
+
+      it "renders the json errors on why the user could not be created" do
+        member_response = JSON.parse(response.body, symbolize_names: true)
+        expect(member_response[:errors][:email]).to include "can't be blank"
+      end
+
+      it { should respond_with 422 }
+    end
+  end
+
 end

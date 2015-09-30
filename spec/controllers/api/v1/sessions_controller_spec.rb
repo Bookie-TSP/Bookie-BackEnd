b9d0afd0c091e3aed8c9cb33308965922b8deee0
Thanyaboon Tovorapan <whatineat@gmail.com>
2015-10-01 03:48:51 +0700

    Added sessions controller create action

---
+++ spec/controllers/api/v1/sessions_controller_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.describe Api::V1::SessionsController, type: :controller do
+	describe "POST #create" do
+
+  	before(:each) do
+  		@member = FactoryGirl.create :member
+  	end
+
+  	context "when the credentials are correct" do
+
+  	  before(:each) do
+  	    credentials = { email: @member.email, password: "12345678" }
+  	    post :create, { session: credentials }
+  	  end
+
+  	  it "returns the user record corresponding to the given credentials" do
+  	    @member.reload
+  	    expect(JSON.parse(response.body, symbolize_names: true)[:auth_token]).to eql @member.auth_token
+  	  end
+
+	    it { should respond_with 200 }
+	  end
+
+  	context "when the credentials are incorrect" do
+
+  	  before(:each) do
+  	    credentials = { email: @member.email, password: "invalidpassword" }
+  	    post :create, { session: credentials }
+  	  end
+
+    	it "returns a json with an error" do
+    	  expect(JSON.parse(response.body, symbolize_names: true)[:errors]).to eql "Invalid email or password"
+    	end
+
+   		it { should respond_with 422 }
+  	end
+	end
+end

69a6e084b8448bc305539f88cb60e0fbe3649340
Thanyaboon Tovorapan <whatineat@gmail.com>
2015-10-01 02:37:47 +0700

    Added user firsts specs

--- spec/models/member_spec.rb
+++ spec/models/member_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Member, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before { @member = FactoryGirl.build(:member) }
+
+  subject { @member }
+
+  it { should respond_to(:email) }
+  it { should respond_to(:password) }
+  it { should respond_to(:password_confirmation) }
+
+  it { should be_valid }
 end

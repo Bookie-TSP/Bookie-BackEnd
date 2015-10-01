8c4fe98a9faebc0d0cfad5ccb61379269c4d2351
Thanyaboon Tovorapan <whatineat@gmail.com>
2015-10-01 15:20:40 +0700

    Removed subdomain

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ require 'api_constraints'
 Rails.application.routes.draw do
   devise_for :members
   # Api definition
-  namespace :api, defaults: { format: :json }, constraints: { subdomain: 'api' }, path: '/' do
+  namespace :api, defaults: { format: :json }, constraints: { }, path: '/api' do
     # We are going to list our resources here
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
       # We are going to list our resources here

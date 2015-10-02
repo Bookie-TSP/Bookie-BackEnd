a7f75f28f14a900ba9f007ab0c9b9ebdf3a7f24f
Thanyaboon Tovorapan <whatineat@gmail.com>
2015-10-02 16:56:49 +0700

    Update response's header

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
-
+  after_filter :set_access_control_headers
   include Authenticable
+
+  def set_access_control_headers
+  	headers['Access-Control-allow-Origin'] = '*'
+  end
 end

cbc4d8e0e07e4aef8feca6693fae90ac0176870b
Thanyaboon Tovorapan <whatineat@gmail.com>
2015-11-11 10:42:02 +0700

    Modified cart API

--- app/controllers/api/v1/members_controller.rb
+++ app/controllers/api/v1/members_controller.rb
@@ -57,60 +57,27 @@ class Api::V1::MembersController < ApplicationController
   end
 
   def create_stock
-    stock_temp = nil
     temp_book = Book.find_by_id(stock_params[:book_id])
     ActiveRecord::Base.transaction do
       if temp_book.nil?
         render json: { errors: 'Book not found' }, status: 422
       else
-        checker = 0
+        if ( !(stock_params[:type] == 'sell') && !(stock_params[:type] == 'lend') )
+          render json: { errors: 'Wrong type' }, status: 422 and return
+        end
         user_line_stocks = current_user.line_stocks
         temp_stock_for_check = Stock.new(stock_params.except(:quantity))
         temp_stock_for_check.member_id = current_user.id
         user_line_stocks.each do |temp_line_stock|
           if eql_attributes?(temp_line_stock.stocks.first, temp_stock_for_check)
-            checker = 1
-            stock_quantity = stock_params[:quantity]
-            params[:stock].delete :quantity
-            (1..stock_quantity).each do |i|
-              stock_temp = temp_line_stock.stocks.build(stock_params)
-              stock_temp.member_id = current_user.id
-              stock_temp.book = temp_book
-            end
-            temp_line_stock.quantity = temp_line_stock.stocks.size
-            if temp_line_stock.save
-              render json: current_user.to_json(:include => [:addresses, :line_stocks]), status: 201, location: [:api, current_user] and return
-            else
-              if stock_temp.save
-                render json: { errors: temp_line_stock.errors }, status: 422 and return
-              else
-                render json: { errors: stock_temp.errors }, status: 422 and return
-              end
-            end
+            stock_creation(temp_book, temp_line_stock, stock_params)
+            return
           end
         end
-        if checker == 0
-          line_stock_temp = current_user.line_stocks.build
-          line_stock_temp.quantity = stock_params[:quantity]
-          line_stock_temp.type = stock_params[:type]
-          params[:stock].delete :quantity
-          (1..line_stock_temp.quantity).each do |i|
-            stock_temp = line_stock_temp.stocks.build(stock_params)
-            stock_temp.member_id = current_user.id
-            stock_temp.book = temp_book
-          end
-          if line_stock_temp.save
-            render json: current_user.to_json(:include => [:addresses, :line_stocks]), status: 201, location: [:api, current_user]
-          else
-            if stock_temp.save
-              render json: { errors: line_stock_temp.errors }, status: 422
-            else
-              render json: { errors: stock_temp.errors }, status: 422
-            end
-          end
-        elsif checker == 1
-          render json: current_user.to_json(:include => [:addresses, :line_stocks]), status: 201, location: [:api, current_user]
-        end
+        line_stock_temp = current_user.line_stocks.build
+        line_stock_temp.type = stock_params[:type]
+        stock_creation(temp_book, line_stock_temp, stock_params)
+        return
       end
     end
   end
@@ -140,11 +107,11 @@ class Api::V1::MembersController < ApplicationController
   end
 
   def get_stock_in_cart
-    render json: current_user.cart.to_json(:include => { :stocks => { :include => :book }}), status: 200
+    render json: current_user.cart.to_json(:include => { :stocks => { :include => :book, :methods => :member }}), status: 200
   end
 
   def get_my_stock
-    render json: current_user.to_json(:include => {:stocks => { :include => :book }}), status: 200
+    render json: current_user.to_json(:include => {:stocks => { :include => :book, :methods => :member }}), status: 200
   end
 
   def destroy
@@ -182,13 +149,28 @@ class Api::V1::MembersController < ApplicationController
 
     def eql_attributes?(old_stock, new_stock)
       meta = [:id, :created_at, :updated_at, :line_stock_id]
-      logger.debug('class1 = ' + old_stock.class.to_s)
-      logger.debug('class2 = ' + new_stock.class.to_s)
       old_stock = old_stock.attributes.symbolize_keys.except(*meta)
       new_stock = new_stock.attributes.symbolize_keys.except(*meta)
-      logger.debug('old = ' + old_stock.to_s)
-      logger.debug('new = ' + new_stock.to_s)
-      logger.debug('result = ' + (old_stock == new_stock).to_s)
       old_stock == new_stock
+    end
+
+    def stock_creation(book, line_stock_being_operated, params)
+      stock_quantity = params[:quantity]
+      logger.debug("Quantity : " + stock_quantity.to_s)
+      (1..stock_quantity).each do |i|
+        stock_temp = line_stock_being_operated.stocks.build(params.except(:quantity))
+        stock_temp.member_id = current_user.id
+        stock_temp.book = book
+      end
+      line_stock_being_operated.quantity = line_stock_being_operated.stocks.size
+      if line_stock_being_operated.save
+        render json: current_user.to_json(:include => [:addresses, :line_stocks]), status: 201, location: [:api, current_user]
+      else
+        if stock_temp.save
+          render json: { errors: line_stock_being_operated.errors }, status: 422
+        else
+          render json: { errors: stock_temp.errors }, status: 422
+        end
+      end
     end
 end
